fe89aee42dd07cc5a10f65215fc4a1d6e67569f6
23ariyar <23ariyar@students.harker.org>
2020-10-06 01:32:05 -0700

    ede

--- Stuck in a Loop/Assets/Scripts/RoundReset.cs
+++ Stuck in a Loop/Assets/Scripts/RoundReset.cs
@@ -12,7 +12,34 @@ public class RoundReset : MonoBehaviour
     public Text roundsDisplayUI;
     public AudioSource audioSource;
 
+    public Sprite backgroundOne;
+    public Sprite backgroundTwo;
+    public Sprite backgroundThree;
+    public Sprite backgroundFour;
+    public Sprite backgroundFive;
+
+    public SpriteRenderer backgroundRendererOne;
+    public SpriteRenderer backgroundRendererTwo;
+    public SpriteRenderer backgroundRendererThree;
+
+
+    private int backgroundIndex = 0;
+    List<Sprite> backgrounds = new List<Sprite>(); //list of sprites
+
     private int roundsCompleted = 0;
+
+    void Start()
+    {
+
+        backgrounds.Add(backgroundOne); //adding ecah sprite to list
+        backgrounds.Add(backgroundTwo);
+        backgrounds.Add(backgroundThree);
+        backgrounds.Add(backgroundFour);
+        backgrounds.Add(backgroundFive);
+
+        InvokeRepeating("SwitchBackground", 0.0f, 12.0f); //every 12 seconds switch background
+
+    }
 
     public void ResetAll()
     {
@@ -56,4 +83,25 @@ public class RoundReset : MonoBehaviour
     {
         roundLoopedImage.SetActive(false);
     }
+
+    public void SwitchBackground()
+    {
+
+        if (backgroundIndex + 1 < 5)
+        { //if there's still more to go
+            backgroundIndex += 1;
+            backgroundRendererOne.sprite = backgrounds[backgroundIndex];
+            backgroundRendererTwo.sprite = backgrounds[backgroundIndex];
+            backgroundRendererThree.sprite = backgrounds[backgroundIndex];
+        }
+        else //otherwise reset the background and call the resetall function in the resetscript
+        {
+            backgroundIndex = 0;
+            backgroundRendererOne.sprite = backgrounds[backgroundIndex];
+            backgroundRendererTwo.sprite = backgrounds[backgroundIndex];
+            backgroundRendererThree.sprite = backgrounds[backgroundIndex];
+            ResetAll();
+        }
+    }
+
 }
